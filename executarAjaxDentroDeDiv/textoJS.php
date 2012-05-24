dd45f744200947c6c06c4ce8847d2549a06ebb8c
Danilo Matias <danilomatias19@gmail.com>
2012-05-24 20:37:12 -0300

    Primeiro Commit

---
+++ executarAjaxDentroDeDiv/textoJS.php
@@ -0,0 +1,6 @@
+<?php
+	header("Content-Type: text/html; charset=ISO-8859-1", true);
+	echo "<script type=\"text/javascript\">window.prompt(\"Prompt\", \"prompt\");</script><br /> Quebra de linha<script>window.alert(\"alert\");</script><br /> oi";
+	/*para que nosso código funcione basta colocar uma tag script e dentro dela  código javascript
+	*/
+?>
